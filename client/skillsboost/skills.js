1e833f7d85c4875a5d64549c885504ba192a56f8
Zeinab Roumieh <zeinabroumieh@outlook.com>
2023-07-03 12:41:21 +0100

    js to show skill classes

--- client/skillsboost/skills.js
+++ client/skillsboost/skills.js
@@ -5,32 +5,39 @@ const showSkills = async () => {
     //     console.log('something went wrong')
     // }
     // const skills = await resp.json()
-    // const { skill_info, skill_image_url } = skills
-    const skill_info = 'skill name'
+    // const { skill_name,skill_info, skill_image_url } = skills
+    
+    const skill_name = 'skill name'
+    const skill_info = 'skill info'
     const skill_image_url = 'https://www.rd.com/wp-content/uploads/2017/01/07_Immune_Surprising_Health_benefits_Gardening_459405181_monkeybusinessimages.jpg'
     
     skillImage.src = skill_image_url
-    skillInfo.textContent = skill_info
-    skillsTable.appendChild(skillRow)
-    skillRow.appendChild(skillColumn)
-    skillColumn.appendChild(skillImage)
-    skillRow.appendChild(skillInfo)
-    skillRow.appendChild(skillColumn)
-    skillColumn.appendChild(applyButton)
+    skillInfo.textContent = `${skill_name} \n ${skill_info}`
+
+    skillsTable.appendChild(row)
+    row.appendChild(skillImageColumn)
+    skillImageColumn.appendChild(skillImage)
+    row.appendChild(skillInfo)
+    row.appendChild(skillButtonColumn)
+    skillButtonColumn.appendChild(applyButton)
 
     applyButton.addEventListener('click',() => {
-        apply(skills)
-    })
+        apply(skill_name)
+    }) 
+
 }
 
-const apply = (skills) => {
-
+const apply = (skill_name) => {
+    //add skill to user in dtb
+    alert(`You have joined the ${skill_name} class!`)
 }
 
 const skillsTable = document.querySelector('#skill-classes')
 
-const skillRow = document.createElement('tr')
-const skillColumn = document.createElement('td')
+const row = document.createElement('tr')
+const skillImageColumn = document.createElement('td')
+const skillButtonColumn = document.createElement('td')
+const skillName = document.createElement('td')
 const skillInfo = document.createElement('td')
 const skillImage = document.createElement('img')
 const applyButton = document.createElement('button')
