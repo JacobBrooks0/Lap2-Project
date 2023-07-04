3599587cac274e74f5cedd7fb5508b96c57e6490
Samin Chowdhury <samin@Samins-Mac-mini.home>
2023-07-04 17:04:13 +0100

    added profile styling

--- client/profile/profile.js
+++ client/profile/profile.js
@@ -10,24 +10,30 @@ async function displayUserData(endpoint, containerId) {
   const userId = getLoggedInUserId();
   const response = await fetch(`http://localhost:3000/users/${userId}/${endpoint}`);
   const data = await response.json();
+  console.log('*************************************************************');
+  console.log(data);  
+  document.getElementById('user-name').innerText = data.name;
+  document.getElementById('user-email').innerText = data.email;
 
   let container = document.getElementById(containerId);
 
   data.forEach(item => {
-    let div = document.createElement('div');
-    
+    let itemContainer = document.createElement('div');
+    itemContainer.className = 'item-container'; 
+
     let title = document.createElement('h3');
     title.innerText = item.name;
-    
+
     let description = document.createElement('p');
     description.innerText = item.info;
-    
-    div.appendChild(title);
-    div.appendChild(description);
-    
-    container.appendChild(div);
+
+    itemContainer.appendChild(title);
+    itemContainer.appendChild(description);
+
+    container.appendChild(itemContainer);
   });
 }
+
 
 function handleLogin(userId) {
   setLoggedInUserId(userId);
