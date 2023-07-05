c2e43779fd4ca9a3801b5c25c71318ce3f856f72
Zeinab Roumieh <zeinabroumieh@outlook.com>
2023-07-05 18:27:06 +0100

    fixed id in events model and started events testing

--- server/__test__/event.test.js
+++ server/__test__/event.test.js
@@ -1,6 +1,77 @@
 const request = require("supertest");
 const app = require("../app");
 const db = require("../database/connect");
+const setupMockDB = require("./setup/setup-mock-db");
+
+let eventID
+let token
+let userID
+
+describe("Community Events Endpoints", () => {
+
+    beforeAll(async () => {
+        setupMockDB()
+
+        const registerDetails = {
+            username: "user",
+            password: "password",
+          }
+
+          await request(app)
+            .post("/users/register")
+            .send(registerDetails)
+
+          const response = await request(app)
+            .post("/users/login")
+            .send(registerDetails)
+          token = response.body.token
+          userID = response.body.user_id
+    })
+
+    afterAll(async () => {
+        await db.end() // Close the database connection
+    })
+    //crud tests
+
+    //read all
+    it("Should get all community events", async() => {
+        const resp = await request(app)
+        .get("/events")
+        .expect(200)
+        
+        expect(Array.isArray(resp.body)).toBe(true)
+        expect(resp.body.length).toBeGreaterThan(0)
+    })
+
+    //create one
+    it("Should create a new event", async() => {
+        const event = {
+            creator_id: userID,
+            name: "Book club",
+            main_image_url: "https://tse4.mm.bing.net/th?id=OIP.jHMwSwX0ZK2kiSGh0zdv6wHaE7&pid=Api",
+            info:"New book club every week",
+            start_date: 1688230800,
+            end_date: 1688230800
+        }
+
+        const resp = await request(app)
+        .post("/events")
+        .set({ authorization: token })
+        .send(event)
+        .expect(201)
+
+        const { event_id } = resp.body
+        eventID = event_id
+
+        expect(resp.body).toHaveProperty("name",event.name)
+    })
+
+    // //read one
+    // it("Should get an event by id", async() => {
+    //     const resp = await request(app)
+    //     .get(`/events/${eventID}/find`)
+    // })
+})
 
 // describe("Community Events Endpoints", () => {
 //   afterAll(async () => {
