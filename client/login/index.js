cbe0d102f9d595d5991e381b3f9a99fbd91b8841
Hussein Raji <111huraji@gmail.com>
2023-07-05 23:42:09 +0100

    create profile setup page and mod login page to accommodate this

--- client/login/index.js
+++ client/login/index.js
@@ -1,27 +1,52 @@
 document.getElementById("login-form").addEventListener("submit", async (e) => {
-    e.preventDefault();
+  e.preventDefault();
 
-    const form = new FormData(e.target);
+  const form = new FormData(e.target);
 
-    const options = {
-        method: "POST",
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            username: form.get("username"),
-            password: form.get("password")
-        })
-    }
+  const options = {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      username: form.get("username"),
+      password: form.get("password"),
+    }),
+  };
 
-    const response = await fetch("http://localhost:3000/users/login", options);
-    const data = await response.json();
+  const response = await fetch("http://localhost:3000/users/login", options);
+  const data = await response.json();
 
-    if (response.status == 200) {
-        localStorage.setItem("token", data.token);
-        window.location.assign("../profile/profile.html");
+  if (response.status == 201) {
+    localStorage.setItem("token", data.token);
+  } else {
+    alert(data.Error);
+  }
+
+  checkForProfileDetails();
+});
+
+async function checkForProfileDetails() {
+  const option = {
+    headers: {
+      Authorization: localStorage.getItem("token"),
+    },
+  };
+  try {
+    const response = await fetch("http://localhost:3000/users/details", option);
+      const data = await response.json();
+      
+    if ((response.status = 200)) {
+      if (!(data.name && data.profile_summary)) {
+        window.location.assign("/client/profile-setup/index.html");
+      } else {
+        window.location.assign("/client/skillsboost/skills.html");
+      }
     } else {
-        alert(data.error);
+      alert(data.Error);
     }
-})
+  } catch (error) {
+    console.log(error);
+  }
+}
---
+++ client/profile-setup/index.js
@@ -0,0 +1,32 @@
+document.getElementById("profile-form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const form = new FormData(e.target);
+
+  console.log([...form.entries()])
+  const options = {
+    method: "PATCH",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      authorization: localStorage.getItem("token"),
+    },
+    body: JSON.stringify({
+      name: form.get("name"),
+      dp_url: form.get("dp_url"),
+      profile_summary: form.get("profile_summary"),
+    }),
+  };
+  try {
+    const result = await fetch("http://localhost:3000/users/update", options);
+    const data = await result.json();
+    console.log(data)
+    if (result.status == 202) {
+      window.location.assign("/client/skillsboost/skills.html");
+    } else {
+      alert(data.Error);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
