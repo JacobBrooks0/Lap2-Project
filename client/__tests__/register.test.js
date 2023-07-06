b1fe485765b3b4c650f923d1aa175ae0af144dce
Zeinab Roumieh <zeinabroumieh@outlook.com>
2023-07-06 23:27:35 +0100

    frontend testing

--- client/__tests__/register.test.js
+++ client/__tests__/register.test.js
@@ -3,20 +3,6 @@ const { renderDOM, mockFetch } = require('./helpers')
 let dom
 let document
 
-// const mockPathname = jest.fn()
-// Object.defineProperty(window, 'location', {
-//     value: {
-//       get pathname() {
-//         return mockPathname();
-//       },
-//     },
-// });
-  
-// mockPathname.mockReturnValue('some');
-
-// const spyWindowOpen = jest.spyOn(window, 'open');
-// spyWindowOpen.mockImplementation(jest.fn());
-
 describe('register/index.html', () => {
     beforeEach(async () => {
         dom = await renderDOM('./register.html')
@@ -29,23 +15,40 @@ describe('register/index.html', () => {
         expect(form).toBeTruthy
     })
 
-    it('Should alert error when nothing is entered', async () => {
-        dom.window.alert = jest.fn()
-        const form = document.querySelector('#register-form')
-        await form.dispatchEvent(new dom.window.Event('submit'))
-        expect(dom.window.alert).toHaveBeenCalledTimes(1)
-    })
-
-    it('Should open login page for correct details', async () => {
-        // location.pathname = jest.fn()
-        dom.window.fetch = mockFetch
-        const form = document.querySelector('#register-form')
+    it('Should show username and password input', async () => {
         const username = document.querySelector('#username')
         username.value = 'username'
         const password = document.querySelector('#password')
         password.value = 'password'
+        expect(username.value).toBe('username')
+        expect(password.value).toBe('password')
+    })
+
+    it('Should alert when username already exists', async () => {
+        dom.window.alert = jest.fn()
+        // jest.spyOn(dom.window,'alert').mockImplementation(()=>{})
+        dom.window.fetch = mockFetch
+        const form = document.querySelector('#register-form')
+        const username = document.querySelector('#username')
+        username.value = 'testing2'
+        const password = document.querySelector('#password')
+        password.value = '1'
         await form.dispatchEvent(new dom.window.Event('submit'))
-        expect(dom.window.location.pathname).toEqual('./login/index.html')
+        expect(dom.window.alert).toHaveBeenCalled()
+    })
+
+    it('Should open login page for correct details', async () => {
+        // global.window = { location: { pathname: null }}
+        dom.window.open = jest.fn()
+        // jest.spyOn(global.window,'open').mockImplementation(()=>{})
+        dom.window.fetch = mockFetch
+        const form = document.querySelector('#register-form')
+        const username = document.querySelector('#username')
+        username.value = 'testing2'
+        const password = document.querySelector('#password')
+        password.value = '1'
+        await form.dispatchEvent(new dom.window.Event('submit'))
+        expect(dom.window.open).toHaveBeenCalled()
     })
 })
 
